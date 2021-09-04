eb6ec8bb64dca286fbc176537332b5c9bfa78177
Dmytro <dpastov@gmail.com>
2021-09-04 12:38:58 +0200

    sign all design documents in database

--- src/main/java/net/prominic/DbSigner/App.java
+++ src/main/java/net/prominic/DbSigner/App.java
@@ -11,6 +11,8 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 import lotus.domino.Database;
+import lotus.domino.Document;
+import lotus.domino.NoteCollection;
 import lotus.domino.NotesException;
 import lotus.domino.NotesFactory;
 import lotus.domino.Session;
@@ -19,8 +21,8 @@ import lotus.notes.addins.JavaServerAddin;
 public class App extends JavaServerAddin {
 	// Constants
 	private final String		JADDIN_NAME				= "DbSigner";
-	private final String		JADDIN_VERSION			= "0.1.0 (first version)";
-	private final String		JADDIN_DATE				= "2021-09-03 16:30";
+	private final String		JADDIN_VERSION			= "0.1.0 (sign all design elements in database)";
+	private final String		JADDIN_DATE				= "2021-09-05 16:30";
 
 	Session 				m_session				= null;
 
@@ -51,31 +53,57 @@ public class App extends JavaServerAddin {
 			logMessage("date         " + this.JADDIN_DATE);
 			logMessage("parameters   " + Arrays.toString(this.args));
 
-			sign();
+			processJSON();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
 
-	private void sign() throws IOException, ParseException, NotesException {
+	private void processJSON() throws IOException, ParseException, NotesException {
 		JSONParser parser = new JSONParser();
 
 		Reader reader = new FileReader("dbsigner.json");
-
 		JSONObject jsonObject = (JSONObject) parser.parse(reader);
-		System.out.println(jsonObject);
 
 		JSONArray dbList = (JSONArray) jsonObject.get("databases");
+		logMessage(String.valueOf(dbList.size()).concat(" - database objects found in JSON"));
+
 		for(Object o : dbList){
 			JSONObject db = (JSONObject) o;
 
 			String filePath = (String) db.get("filepath");
-			System.out.println(filePath);
 
 			Database database = m_session.getDatabase(null, filePath);
-			System.out.println(database.getTitle() + " - sign?");
-			database.sign();
+			if (database != null && database.isOpen()) {
+				sign(database);
+			}
+			else {
+				this.logMessage(filePath.concat(" - can't be initialized"));
+			}
 		}
+
+		reader.close();
+	}
+
+	private void sign(Database database) throws NotesException {
+		logMessage(database.getTitle().concat(" - is going to be signed"));
+
+		NoteCollection nc = database.createNoteCollection(true);
+		nc.buildCollection();
+
+		String noteid = nc.getFirstNoteID();
+		while (noteid.length() > 0) {
+			Document doc = database.getDocumentByID(noteid);
+
+			doc.sign();
+			doc.save();
+			doc.recycle();
+
+			noteid = nc.getNextNoteID(noteid);
+		}
+
+		database.recycle();
+		logMessage(database.getTitle().concat(" - has been signed (").concat(String.valueOf(nc.getCount())) + " design elements)");
 	}
 
 	/**

f05c9fec96532e5efbd40f1ffe34a679d542cd7b
Dmytro <dpastov@gmail.com>
2022-02-22 14:24:00 +0100

    adjusted DbSigner to sign database using command line in Domino

--- src/main/java/DbSigner.java
+++ src/main/java/DbSigner.java
@@ -1,11 +1,6 @@
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.Reader;
 import java.util.Arrays;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+import java.util.Calendar;
+
 import lotus.domino.Database;
 import lotus.domino.Document;
 import lotus.domino.NoteCollection;
@@ -13,17 +8,24 @@ import lotus.domino.NotesException;
 import lotus.domino.NotesFactory;
 import lotus.domino.Session;
 import lotus.notes.addins.JavaServerAddin;
+import lotus.notes.internal.MessageQueue;
 
 public class DbSigner extends JavaServerAddin {
 	// Constants
 	private final String		JADDIN_NAME				= "DbSigner";
-	private final String		JADDIN_VERSION			= "0.1.0 (sign all design elements in database)";
-	private final String		JADDIN_DATE				= "2021-09-05 16:30";
+	private final String		JADDIN_VERSION			= "0.2.0 (sign all design elements in a database)";
+	private final String		JADDIN_DATE				= "2022-02-22 16:30";
 
-	Session 				m_session				= null;
+	// MessageQueue Constants
+	// Message Queue name for this Addin (normally uppercase);
+	// MSG_Q_PREFIX is defined in JavaServerAddin.class
+	private static final int 	MQ_MAX_MSGSIZE 			= 1024;
+	private final String 		qName 					= MSG_Q_PREFIX + JADDIN_NAME.toUpperCase();
+	MessageQueue 				mq						= null;
+	Session 					m_session				= null;
 
-	private String[] 		args 					= null;
-	private int 			dominoTaskID			= 0;
+	private String[] 			args 					= null;
+	private int 				dominoTaskID			= 0;
 
 	// constructor if parameters are provided
 	public DbSigner(String[] args) {
@@ -35,8 +37,7 @@ public class DbSigner extends JavaServerAddin {
 	/* the runNotes method, which is the main loop of the Addin */
 	@Override
 	public void runNotes () {
-		setAddinState("Initializing");
-
+		// Set the Java thread name to the class name (default would be "Thread-n")
 		this.setName(JADDIN_NAME);
 
 		// Create the status line showed in 'Show Task' console command
@@ -45,61 +46,127 @@ public class DbSigner extends JavaServerAddin {
 		try {
 			m_session = NotesFactory.createSession();
 
-			logMessage("version      " + this.JADDIN_VERSION);
-			logMessage("date         " + this.JADDIN_DATE);
-			logMessage("parameters   " + Arrays.toString(this.args));
+			showInfo();
 
-			processJSON();
+			listen();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
 
-	private void processJSON() throws IOException, ParseException, NotesException {
-		JSONParser parser = new JSONParser();
+	private void listen() {
+		StringBuffer qBuffer = new StringBuffer(1024);
 
-		Reader reader = new FileReader("dbsigner.json");
-		JSONObject jsonObject = (JSONObject) parser.parse(reader);
-
-		JSONArray dbList = (JSONArray) jsonObject.get("databases");
-		logMessage(String.valueOf(dbList.size()).concat(" - database objects found in JSON"));
-
-		for(Object o : dbList){
-			JSONObject db = (JSONObject) o;
-
-			String filePath = (String) db.get("filepath");
-
-			Database database = m_session.getDatabase(null, filePath);
-			if (database != null && database.isOpen()) {
-				sign(database);
+		try {
+			mq = new MessageQueue();
+			int messageQueueState = mq.create(qName, 0, 0);	// use like MQCreate in API
+			if (messageQueueState == MessageQueue.ERR_DUPLICATE_MQ) {
+				logMessage(this.JADDIN_NAME + " task is already running");
+				return;
 			}
-			else {
-				this.logMessage(filePath.concat(" - can't be initialized"));
+
+			if (messageQueueState != MessageQueue.NOERROR) {
+				logMessage("Unable to create the Domino message queue");
+				return;
 			}
+
+			if (mq.open(qName, 0) != MessageQueue.NOERROR) {
+				logMessage("Unable to open Domino message queue");
+				return;
+			}
+
+			setAddinState("Idle");
+			while (this.addInRunning() && (messageQueueState != MessageQueue.ERR_MQ_QUITTING)) {
+				/* gives control to other task in non preemptive os*/
+				OSPreemptOccasionally();
+
+				// check for command from console
+				messageQueueState = mq.get(qBuffer, MQ_MAX_MSGSIZE, MessageQueue.MQ_WAIT_FOR_MSG, 1000);
+				if (messageQueueState == MessageQueue.ERR_MQ_QUITTING) {
+					return;
+				}
+
+				// check messages for Genesis
+				String cmd = qBuffer.toString().trim();
+				if (!cmd.isEmpty()) {
+					resolveMessageQueueState(cmd);
+				};
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
 		}
-
-		reader.close();
 	}
 
-	private void sign(Database database) throws NotesException {
-		logMessage(database.getTitle().concat(" - is going to be signed"));
+	private void resolveMessageQueueState(String cmd) {
+		if ("-h".equals(cmd) || "help".equals(cmd)) {
+			showHelp();
+		}
+		else if ("info".equals(cmd)) {
+			showInfo();
+		}
+		else if (cmd.startsWith("sign")) {
+			sign(cmd);
+		}
+		else {
+			logMessage("Command is not recognized (use -h or help to get details)");
+		}
+	}
 
-		NoteCollection nc = database.createNoteCollection(true);
-		nc.buildCollection();
+	private void showInfo() {
+		logMessage("version      " + this.JADDIN_VERSION);
+		logMessage("date         " + this.JADDIN_DATE);
+		logMessage("parameters   " + Arrays.toString(this.args));
+	}
 
-		String noteid = nc.getFirstNoteID();
-		while (noteid.length() > 0) {
-			Document doc = database.getDocumentByID(noteid);
+	private void showHelp() {
+		int year = Calendar.getInstance().get(Calendar.YEAR);
+		logMessage("*** Usage ***");
+		AddInLogMessageText("load runjava DbSigner");
+		AddInLogMessageText("tell DbSigner <command>");
+		AddInLogMessageText("   quit             Unload DbSigner");
+		AddInLogMessageText("   help             Show help information (or -h)");
+		AddInLogMessageText("   info             Show version and more of DbSigner");
+		AddInLogMessageText("   sign <filepath>  Sign all design elements in filepath");
+		AddInLogMessageText("Copyright (C) Prominic.NET, Inc. 2021" + (year > 2021 ? " - " + Integer.toString(year) : ""));
+		AddInLogMessageText("See https://prominic.net for more details.");
+	}
 
-			doc.sign();
-			doc.save();
-			doc.recycle();
+	private void sign(String cmd) {
+		try {
 
-			noteid = nc.getNextNoteID(noteid);
+			String[] optArr = cmd.split("\\s+");
+			if (optArr.length < 2) {
+				logMessage("sign command should follow with <filepath> as a parameter, see help for more information");
+				return;
+			}
+
+			Database database = m_session.getDatabase(null, optArr[1]);
+			if (database == null) {
+				logMessage("database not found: " + optArr[1]);
+				return;
+			}
+			logMessage(database.getTitle().concat(" - is going to be signed (please wait a bit)"));
+
+			NoteCollection nc = database.createNoteCollection(true);
+			nc.buildCollection();
+
+			String noteid = nc.getFirstNoteID();
+			while (noteid.length() > 0) {
+				Document doc = database.getDocumentByID(noteid);
+
+				doc.sign();
+				doc.save();
+				doc.recycle();
+
+				noteid = nc.getNextNoteID(noteid);
+			}
+
+			logMessage(database.getTitle().concat(" - has been signed (").concat(String.valueOf(nc.getCount())) + " design elements)");
+			database.recycle();
+		} catch (NotesException e) {
+			logMessage("sign command failed: " + e.getMessage());
 		}
 
-		logMessage(database.getTitle().concat(" - has been signed (").concat(String.valueOf(nc.getCount())) + " design elements)");
-		database.recycle();
 	}
 
 	/**

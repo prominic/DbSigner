39d85fec7f0c8d67e3f1f7aba4b87e389bf8a9d1
Dmytro <dpastov@gmail.com>
2022-03-01 14:04:36 +0100

    created core-abstract class

--- src/main/java/DbSigner.java
+++ src/main/java/DbSigner.java
@@ -1,5 +1,3 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.util.Arrays;
 import java.util.Calendar;
 
@@ -7,119 +5,37 @@ import lotus.domino.Database;
 import lotus.domino.Document;
 import lotus.domino.NoteCollection;
 import lotus.domino.NotesException;
-import lotus.domino.NotesFactory;
-import lotus.domino.Session;
-import lotus.notes.addins.JavaServerAddin;
-import lotus.notes.internal.MessageQueue;
 
-public class DbSigner extends JavaServerAddin {
-	// Constants
-	private final String		JADDIN_NAME				= "DbSigner";
-	private final String		JADDIN_VERSION			= "0.2.0 (working version)";
-	private final String		JADDIN_DATE				= "2022-02-23 14:30";
-
-	// MessageQueue Constants
-	// Message Queue name for this Addin (normally uppercase);
-	// MSG_Q_PREFIX is defined in JavaServerAddin.class
-	private static final int 	MQ_MAX_MSGSIZE 			= 1024;
-	private final String 		qName 					= MSG_Q_PREFIX + JADDIN_NAME.toUpperCase();
-	MessageQueue 				mq						= null;
-	Session 					m_session				= null;
-
-	private String[] 			args 					= null;
-	private int 				dominoTaskID			= 0;
-
-	// constructor if parameters are provided
-	public DbSigner(String[] args) {
-		this.args = args;
-	}
-
-	public DbSigner() {}
-
-	/* the runNotes method, which is the main loop of the Addin */
+public class DbSigner extends JavaServerAddinGenesis {
 	@Override
-	public void runNotes () {
-		// Set the Java thread name to the class name (default would be "Thread-n")
-		this.setName(JADDIN_NAME);
-
-		// Create the status line showed in 'Show Task' console command
-		this.dominoTaskID = createAddinStatusLine(this.JADDIN_NAME);
-
-		try {
-			m_session = NotesFactory.createSession();
-
-			showInfo();
-
-			listen();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+	protected String getJavaAddinName() {
+		return "DbSigner";
+	}
+	
+	@Override
+	protected String getJavaAddinVersion() {
+		return "0.3.0 (base class)";
+	}
+	
+	@Override
+	protected String getJavaAddinDate() {
+		return "2022-03-01 14:30";
 	}
 
-	private void listen() {
-		StringBuffer qBuffer = new StringBuffer(1024);
-
-		try {
-			mq = new MessageQueue();
-			int messageQueueState = mq.create(qName, 0, 0);	// use like MQCreate in API
-			if (messageQueueState == MessageQueue.ERR_DUPLICATE_MQ) {
-				logMessage(this.JADDIN_NAME + " task is already running");
-				return;
-			}
-
-			if (messageQueueState != MessageQueue.NOERROR) {
-				logMessage("Unable to create the Domino message queue");
-				return;
-			}
-
-			if (mq.open(qName, 0) != MessageQueue.NOERROR) {
-				logMessage("Unable to open Domino message queue");
-				return;
-			}
-
-			setAddinState("Idle");
-			while (this.addInRunning() && (messageQueueState != MessageQueue.ERR_MQ_QUITTING)) {
-				/* gives control to other task in non preemptive os*/
-				OSPreemptOccasionally();
-
-				// check for command from console
-				messageQueueState = mq.get(qBuffer, MQ_MAX_MSGSIZE, MessageQueue.MQ_WAIT_FOR_MSG, 1000);
-				if (messageQueueState == MessageQueue.ERR_MQ_QUITTING) {
-					return;
-				}
-
-				// check messages for Genesis
-				String cmd = qBuffer.toString().trim();
-
-				if (!cmd.isEmpty()) {
-					resolveMessageQueueState(cmd);
-				};
-
-				BufferedReader reader = new BufferedReader(new FileReader("JavaAddin\\DbSigner.txt"));
-				String line = reader.readLine();
-				while (line != null) {
-					System.out.println(line);
-					
-					if (!line.isEmpty()) {
-						resolveMessageQueueState(cmd);
-					}
-					
-					// read next line
-					line = reader.readLine();
-				}
-				reader.close();
-			}
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+	@Override
+	protected String getCmdFileName() {
+		return "dbsigner.txt";
 	}
 
-	private void resolveMessageQueueState(String cmd) {
+	protected void resolveMessageQueueState(String cmd) {
 		if ("-h".equals(cmd) || "help".equals(cmd)) {
 			showHelp();
 		}
 		else if ("info".equals(cmd)) {
 			showInfo();
+		}
+		else if ("quit".equals(cmd)) {
+			quit();
 		}
 		else if (cmd.length()>0) {
 			sign(cmd);
@@ -129,9 +45,9 @@ public class DbSigner extends JavaServerAddin {
 		}
 	}
 
-	private void showInfo() {
-		logMessage("version      " + this.JADDIN_VERSION);
-		logMessage("date         " + this.JADDIN_DATE);
+	protected void showInfo() {
+		logMessage("version      " + this.getJavaAddinName());
+		logMessage("date         " + this.getJavaAddinDate());
 		logMessage("parameters   " + Arrays.toString(this.args));
 	}
 
@@ -175,74 +91,12 @@ public class DbSigner extends JavaServerAddin {
 			}
 
 			logMessage(database.getTitle().concat(" - has been signed (").concat(String.valueOf(nc.getCount())) + " design elements)");
+			
+			nc.recycle();
 			database.recycle();
 		} catch (NotesException e) {
 			logMessage("sign command failed: " + e.getMessage());
 		}
-
 	}
 
-	/**
-	 * Write a log message to the Domino console. The message string will be prefixed with the add-in name
-	 * followed by a column, e.g. <code>"AddinName: xxxxxxxx"</code>
-	 *
-	 * @param	message		Message to be displayed
-	 */
-	private final void logMessage(String message) {
-		AddInLogMessageText(this.JADDIN_NAME + ": " + message, 0);
-	}
-
-	/**
-	 * Set the text of the add-in which is shown in command <code>"show tasks"</code>.
-	 *
-	 * @param	text	Text to be set
-	 */
-	private final void setAddinState(String text) {
-
-		if (this.dominoTaskID == 0)
-			return;
-
-		AddInSetStatusLine(this.dominoTaskID, text);
-	}
-
-	/**
-	 * Create the Domino task status line which is shown in <code>"show tasks"</code> command.
-	 *
-	 * Note: This method is also called by the JAddinThread and the user add-in
-	 *
-	 * @param	name	Name of task
-	 * @return	Domino task ID
-	 */
-	public final int createAddinStatusLine(String name) {
-		return (AddInCreateStatusLine(name));
-	}
-
-	@Override
-	public void termThread() {
-		terminate();
-
-		super.termThread();
-	}
-
-	/**
-	 * Terminate all variables
-	 */
-	private void terminate() {
-		try {
-			AddInDeleteStatusLine(dominoTaskID);
-
-			if (this.m_session != null) {
-				this.m_session.recycle();
-				this.m_session = null;
-			}
-			if (this.mq != null) {
-				this.mq.close(0);
-				this.mq = null;
-			}
-
-			logMessage("UNLOADED (OK) " + JADDIN_VERSION);
-		} catch (NotesException e) {
-			logMessage("UNLOADED (**FAILED**) " + JADDIN_VERSION);
-		}
-	}
 }

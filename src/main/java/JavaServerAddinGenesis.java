f817e2c49bd045bc3833bac91f4bda61c0cf11ca
Dmytro <dpastov@gmail.com>
2022-03-01 14:36:34 +0100

    added base class

---
+++ src/main/java/JavaServerAddinGenesis.java
@@ -0,0 +1,189 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+import lotus.domino.NotesException;
+import lotus.domino.NotesFactory;
+import lotus.domino.Session;
+import lotus.notes.addins.JavaServerAddin;
+import lotus.notes.internal.MessageQueue;
+
+abstract class JavaServerAddinGenesis extends JavaServerAddin {
+
+	// MessageQueue Constants
+	// Message Queue name for this Addin (normally uppercase);
+	// MSG_Q_PREFIX is defined in JavaServerAddin.class
+	protected static final int 	MQ_MAX_MSGSIZE 			= 1024;
+	protected MessageQueue 		mq						= null;
+	protected Session 			m_session				= null;
+
+	protected String[] 			args 					= null;
+	private int 				dominoTaskID			= 0;
+
+	// constructor if parameters are provided
+	public JavaServerAddinGenesis(String[] args) {
+		this.args = args;
+	}
+
+	public JavaServerAddinGenesis() {}
+	
+	protected abstract String getJavaAddinName();
+	protected abstract String getJavaAddinVersion();
+	protected abstract String getJavaAddinDate();
+	protected abstract void showInfo();
+	protected abstract void resolveMessageQueueState(String cmd);
+	protected abstract String getCmdFileName();
+	
+	protected String getQName() {
+		return MSG_Q_PREFIX + getJavaAddinName().toUpperCase();
+	}
+	
+	/* the runNotes method, which is the main loop of the Addin */
+	@Override
+	public void runNotes () {
+		// Set the Java thread name to the class name (default would be "Thread-n")
+		this.setName(this.getJavaAddinName());
+
+		// Create the status line showed in 'Show Task' console command
+		this.dominoTaskID = createAddinStatusLine(this.getJavaAddinName());
+
+		try {
+			m_session = NotesFactory.createSession();
+
+			showInfo();
+
+			listen();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	protected void listen() {
+		StringBuffer qBuffer = new StringBuffer(MQ_MAX_MSGSIZE);
+
+		try {
+			mq = new MessageQueue();
+			int messageQueueState = mq.create(this.getQName(), 0, 0);	// use like MQCreate in API
+			if (messageQueueState == MessageQueue.ERR_DUPLICATE_MQ) {
+				logMessage(this.getJavaAddinName() + " task is already running");
+				return;
+			}
+
+			if (messageQueueState != MessageQueue.NOERROR) {
+				logMessage("Unable to create the Domino message queue");
+				return;
+			}
+
+			if (mq.open(this.getQName(), 0) != MessageQueue.NOERROR) {
+				logMessage("Unable to open Domino message queue");
+				return;
+			}
+
+			setAddinState("Idle");
+			while (this.addInRunning() && (messageQueueState != MessageQueue.ERR_MQ_QUITTING)) {
+				/* gives control to other task in non preemptive os*/
+				OSPreemptOccasionally();
+
+				// check for command from console
+				messageQueueState = mq.get(qBuffer, MQ_MAX_MSGSIZE, MessageQueue.MQ_WAIT_FOR_MSG, 1000);
+				if (messageQueueState == MessageQueue.ERR_MQ_QUITTING) {
+					return;
+				}
+
+				// check messages for Genesis
+				String cmd = qBuffer.toString().trim();
+
+				if (!cmd.isEmpty()) {
+					resolveMessageQueueState(cmd);
+				};
+
+				// execute commands from file
+				File file = new File(this.getCmdFileName());
+				if (file.exists()) {
+					BufferedReader br = new BufferedReader(new FileReader(file));
+					String line;
+					while((line = br.readLine()) != null) {
+						System.out.println(line);
+						
+						if (!line.isEmpty()) {
+							resolveMessageQueueState(line);
+						}
+					}
+					
+					br.close();
+					file.delete();
+				}
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	protected void quit() {
+		this.stopAddin();
+	}
+	
+	/**
+	 * Write a log message to the Domino console. The message string will be prefixed with the add-in name
+	 * followed by a column, e.g. <code>"AddinName: xxxxxxxx"</code>
+	 *
+	 * @param	message		Message to be displayed
+	 */
+	protected final void logMessage(String message) {
+		AddInLogMessageText(this.getJavaAddinName() + ": " + message, 0);
+	}
+
+	/**
+	 * Set the text of the add-in which is shown in command <code>"show tasks"</code>.
+	 *
+	 * @param	text	Text to be set
+	 */
+	protected final void setAddinState(String text) {
+
+		if (this.dominoTaskID == 0)
+			return;
+
+		AddInSetStatusLine(this.dominoTaskID, text);
+	}
+
+	/**
+	 * Create the Domino task status line which is shown in <code>"show tasks"</code> command.
+	 *
+	 * Note: This method is also called by the JAddinThread and the user add-in
+	 *
+	 * @param	name	Name of task
+	 * @return	Domino task ID
+	 */
+	protected final int createAddinStatusLine(String name) {
+		return (AddInCreateStatusLine(name));
+	}
+
+	@Override
+	public void termThread() {
+		terminate();
+
+		super.termThread();
+	}
+
+	/**
+	 * Terminate all variables
+	 */
+	protected void terminate() {
+		try {
+			AddInDeleteStatusLine(dominoTaskID);
+
+			if (this.m_session != null) {
+				this.m_session.recycle();
+				this.m_session = null;
+			}
+			if (this.mq != null) {
+				this.mq.close(0);
+				this.mq = null;
+			}
+
+			logMessage("UNLOADED (OK) " + this.getJavaAddinVersion());
+		} catch (NotesException e) {
+			logMessage("UNLOADED (**FAILED**) " + this.getJavaAddinVersion());
+		}
+	}
+}
